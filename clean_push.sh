d4623d5a3a9e866de9c3850aef425dc87f27fe3c
BLACKBOX.254 <piotrtrzosinski3@gmail.com>
2025-05-10 03:57:45 +0200

    Czysty commit bez tokenów

--- clean_push.sh
+++ clean_push.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
-echo -e "\n[1] Usuwam poufne dane z historii..."
+echo "[1] Usuwam poufne dane z historii..."
 git filter-branch --force --index-filter \
   "git rm --cached --ignore-unmatch WARSTWY/skrypt.sh" \
   --prune-empty --tag-name-filter cat -- --all
 
-echo -e "\n[2] Dodaję zmiany..."
+echo "[2] Dodaję zmiany..."
 git add .
 git commit -m "Czysty commit bez tokenów"
 
-echo -e "\n[3] Wypycham z nadpisaniem..."
+echo "[3] Wypycham z nadpisaniem..."
 git push --force --set-upstream origin main
